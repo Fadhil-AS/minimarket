397ac4aff826019d80a36c0f5ec0b9d47954c603
Fadhil-AS <mfadhil661@gmail.com>
2021-04-18 22:42:51 +0700

    -

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\MinimarketController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\PemasokController;
+use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PembelianController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +27,29 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => 'auth'], function(){
+    // Dashboard
+    Route::get('/', [MinimarketController::class, "index"]);
+
+    // Barang
+    Route::resource('/barang', BarangController::class)->except('create', 'show', 'edit');
+
+    // produk
+    Route::resource('/produk', ProdukController::class)->except('create', 'show', 'edit');
+
+    // pelanggan
+    Route::resource('/pelanggan', PelangganController::class)->except('create', 'show', 'edit');
+
+    // Pemasokan
+    Route::resource('/pemasokan', PemasokController::class)->except('create', 'show', 'edit');
+
+    // Users
+    Route::get('/admin/users', [UserController::class, "index"]);
+    Route::post('/admin/users', [UserController::class, "store"]);
+    Route::put('/admin/users/update/{id}', [UserController::class, "update"]);
+    Route::delete('/admin/users/{id}', [UserController::class, "destroy"]);
+
+    // Pembelian
+    Route::get('/admin/pembelian', [PembelianController::class, "index"]);
+});
