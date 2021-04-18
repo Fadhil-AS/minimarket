c226173d38f0df4522518115de378f8f806556e1
Fadhil-AS <mfadhil661@gmail.com>
2021-04-18 20:52:39 +0700

    Menambahkan seeder pada users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
 class UserSeeder extends Seeder
@@ -15,5 +17,12 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::factory(10)->create();
+        $data = [
+            'name' => 'Muhammad Fadhil Ardiansyah S',
+            'email' => 'mfadhil@test.com',
+            'password' => Hash::make('fadhil123'),
+            'level' => 'admin'
+        ];
+        DB::table('users')->insert($data);
     }
 }
