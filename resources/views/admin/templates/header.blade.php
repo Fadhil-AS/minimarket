de70783358bbcdab0d59e2994ed998892fdf5f21
Fadhil-AS <mfadhil661@gmail.com>
2021-04-26 13:33:28 +0700

    -

--- resources/views/admin/templates/header.blade.php
+++ resources/views/admin/templates/header.blade.php
@@ -8,8 +8,7 @@
     <link href="{{asset('assets')}}/css/styles.css" rel="stylesheet" />
     <link rel="stylesheet" href="{{asset('assets')}}/DataTables/datatables.min.css">
     <link rel="stylesheet" href="{{asset('assets')}}/fontawesome/css/all.css">
-    <script src="{{asset('assets')}}/swal2/dist/sweetalert2.min.js"></script>
-    <link rel="stylesheet" href="{{asset('assets')}}/swal2/dist/sweetalert2.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/sweetalert2/package/dist/sweetalert2.min.css">
     @stack('style')
 </head>
 
--- resources/views/admin/templates/script.blade.php
+++ resources/views/admin/templates/script.blade.php
@@ -1,10 +1,9 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="{{asset('assets')}}/js/jquery-3.6.0.min.js"></script>
+<script src="{{asset('assets')}}/bootstrap/js/bootstrap.bundle.js"></script>
 <script src="{{asset('assets')}}/js/scripts.js"></script>
 <script src="{{asset('assets')}}/DataTables/datatables.min.js"></script>
 <script src="{{asset('assets')}}/assets/demo/datatables-demo.js"></script>
 {{-- swal --}}
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-{{-- <script src="{{asset('assets')}}/swal2/dist/sweetalert2.all.min.js"></script> --}}
+<script src="{{asset('assets')}}/sweetalert2/package/dist/sweetalert2.min.js"></script>
 <script src="{{asset('assets')}}/fontawesome/js/all.js"></script>
 @stack('script')
